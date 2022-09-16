5b4859dda7fa62219633ad4442d0cfea49aeb32a
Ivan Falcon <rz.crytek@gmail.com>
2022-09-16 14:42:13 -0500

    remove import card.styled

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 export * from './Button';
 export { default as Loader } from './Loader';
 export { default as Card } from './card/Card';
-export * from './card/card.styled';
 export { default as Footer } from './footer/Footer';
 export { default as Header } from './header/Header';
