215e3264c0d46f75339418c63355046190bb7375
Ivan Falcon <rz.crytek@gmail.com>
2022-09-16 15:15:52 -0500

    error-build-vercel: prueba - elimino import card

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,4 @@
-import Card from './card/Card';
-
 export * from './Button';
 export { default as Loader } from './Loader';
 export { default as Footer } from './footer/Footer';
 export { default as Header } from './header/Header';
-
-export { Card };
